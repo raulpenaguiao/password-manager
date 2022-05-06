ae4c1fe0f9131712a4bf002f22ba1e04e7f5f473
Raul Penaguiao <raul.penegas@gmail.com>
2022-05-06 14:30:13 -0700

    initializing my password manager

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+selectDropdownHTML = document.querySelector("#selectOldPasswords");
+
+//Put all old passwords in the DOM
+
+
+class pass{
+    constructor(name, email, user, password, text){
+        this._name = name;
+        this._email = email;
+        this._user = user;
+        this._password = password;
+        this._text = text;
+    }
+    
+}
+
+
+
+
+
+
